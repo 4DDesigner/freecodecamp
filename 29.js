db83c01ad1ff852118b8d982df511a92b98b7d2f
4DDesigner <95ejey@mail.ru>
2023-12-16 22:57:14 +0300

    29.js

---
+++ 29.js
@@ -0,0 +1,5 @@
+// Setup
+const lastName = "Lovelace";
+const thirdLeterOfLastName = lastName[2];
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
