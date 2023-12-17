a1db4d89d4f9f2de00457996b9d468f3477864c8
4DDesigner <95ejey@mail.ru>
2023-12-17 11:06:37 +0300

    32.js

---
+++ 32.js
@@ -0,0 +1,6 @@
+// Setup
+let firstLetterOfLastName = "1";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+// Only change code below this line
+firstLetterOfLastName = firstLetterOfLastName; // Change this line
