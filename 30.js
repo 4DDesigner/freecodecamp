def4da8f1740ced7355e7bec67716b75384acc1a
4DDesigner <95ejey@mail.ru>
2023-12-17 10:47:29 +0300

    30.js

---
+++ 30.js
@@ -0,0 +1,5 @@
+// Setup
+const lastName = "Lovelace";
+const lastLetter = lastName[lastName.length - 1];
+// Only change code below this line
+const lastLetterOfLastName = lastLetter; // Change this line
